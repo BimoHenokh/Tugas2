60d19b92fc79fa431a01b390ebab34f9397bf715
bimo.henokh <bimo.henokh@ui.ac.id>
2022-09-13 22:28:49 +0700

    commit pertama sebelum html

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html")
