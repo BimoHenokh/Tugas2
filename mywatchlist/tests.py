e457ff67426f0c787a2ac16ccff137dd05680af1
bimo.henokh <bimo.henokh@ui.ac.id>
2022-09-22 11:03:40 +0700

    testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,22 +1,21 @@
-from multiprocessing.connection import Client
 from unittest import TestCase
-import unittest
-from urllib import response
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
+from django.test import Client
 from mywatchlist.models import MyWatchlistItem
 from mywatchlist.views import show_html, show_json, show_xml
-from mywatchlist.urls import urlpatterns
 
-# class TestUrls(SimpleTestCase):
-#     def test_url_html(self):
-#         url = reverse('show_html')
-#         print(resolve(url))
-
-class UrlTest(unittest.TestCase):
-
-    def setUp(self):
-        self.client = Client()
+class UrlTest(TestCase):
 
     def test_url_html(self):
-        response = self.client.get('/mywatchlist/html/')
+        client = Client()
+        response = client.get('/mywatchlist/html/')
+        response.status_code
+
+    def test_url_xml(self):
+        client = Client()
+        response = client.get('/mywatchlist/xml/')
+        response.status_code
+
+    def test_url_json(self):
+        client = Client()
+        response = client.get('/mywatchlist/json/')
+        response.status_code
